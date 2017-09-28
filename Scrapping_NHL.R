d76c8d79f6fa4b17b4e8852bc58d9f77dd384bdd
mzakariaCERN <mohammed.zakaria@catapultsports.com>
2017-09-27 23:43:31 -0500

    20170927 Added more columns to be scrapped from the website

--- Scrapping_NHL.R
+++ Scrapping_NHL.R
@@ -3,7 +3,10 @@ library('rvest')
 ## below we set the link to the game page (notice the '')
 #url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES030411.HTM'
 #url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES030133.HTM'
-url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES021108.HTM'
+#url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES021108.HTM'
+#url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES020143.HTM'
+url <- 'http://www.nhl.com/scores/htmlreports/20162017/ES020011.HTM'
+
 webpage <- read_html(url)
 webpage
 Full_data <-  html_nodes(webpage,'.rborder')
@@ -20,9 +23,11 @@ RefMonth <- substring(RefDate, 6, 7)
 RefDay <- substring(RefDate, 9, 11)
 
 Full_DateAccurae_txt[14] ## the string containing the date
-RefDate <- regmatches(Full_DateAccurae_txt[14], regexpr("[0-9][0-9], [0-9][0-9][0-9][0-9]", Full_DateAccurae_txt[14]))
-RefYear <- substring(RefDate, 5, 8)
+RefDate <- regmatches(Full_DateAccurae_txt[14], regexpr("[0-9]+, [0-9][0-9][0-9][0-9]", Full_DateAccurae_txt[14]))
+RefYear <- regmatches(RefDate, regexpr(", [0-9][0-9][0-9][0-9]", RefDate))
+RefYear <- substring(RefYear, 3, 8)
 RefDay <- substring(RefDate, 1,2)
+RefDay <- regmatches(RefDay, regexpr("[0-9]+", RefDay))
 ToGetMonth <- regmatches(Full_DateAccurae_txt[14], regexpr(", [A-Z][a-z]+", Full_DateAccurae_txt[14]))
 RefMonth_2 <- regmatches(ToGetMonth, regexpr("[A-Z][a-z]+", ToGetMonth))
 RefMonth_2 <- substring(RefMonth_2, 1, 3)
@@ -31,24 +36,25 @@ mo2Num <- function(x) match(tolower(x), tolower(month.abb))
 RefMonth_2 <- mo2Num(RefMonth_2)
 
 RefDateProper <- paste(RefMonth_2, "/", RefDay, "/", RefYear)
+RefDateProper <- gsub(" ", "", RefDateProper, fixed = TRUE)
 testing <- as.data.frame(Full_data_txt)[27:525,1]
 ## here we set the patch for the csv files to be saved (notice "")
 setwd("C:/Users/Mohammed/Desktop/EARL_Tutorial/Scrapping")
 #as.matrix(testing, ncol = 25)
 tableGoodHome <- as.data.frame(matrix(testing,ncol =25,byrow = T))
-tableUsefulHome <- tableGoodHome[, c(1, 2, 3, 10, 12)]
+tableUsefulHome <- tableGoodHome[, c(1, 2, 3, 10, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24)]
 tableUsefulHome <- cbind(tableUsefulHome, RefDateProper)
-colnames(tableUsefulHome) <- c("Number", "Pos", "Name", "GT", "PP", "Date")
+colnames(tableUsefulHome) <- c("Number", "Pos", "Name", "GT", "Ave", "PP" , "SH" , "EV", "S", "a/b", "MS", "HT", "FW" ,"FL","Date")
 write.csv(tableUsefulHome , file = "Home.csv")
 
 visiting <- as.data.frame(Full_data_txt)[576:1075,1]
 #setwd("C:/Users/Mohammed/Desktop/EARL_Tutorial/Scrapping")
 #as.matrix(visiting, ncol = 25)
 tableVisiting <- as.data.frame(matrix(visiting,ncol =25,byrow = T))
-tableUsefulVisiting <- tableVisiting[, c(1, 2, 3, 10, 12)]
+tableUsefulVisiting <- tableVisiting[, c(1, 2, 3, 10, 12, 13, 14, 15, 16, 17, 18, 19, 23, 24)]
 
 tableUsefulVisiting <- cbind(tableUsefulVisiting, RefDateProper)
-colnames(tableUsefulVisiting) <- c("Number", "Pos", "Name", "GT", "PP", "Date")
+colnames(tableUsefulVisiting) <- c("Number", "Pos", "Name", "GT", "Ave", "PP", "SH" , "EV", "S" , "a/b", "MS", "HT", "FW", "FL", "Date")
 
 write.csv(tableUsefulVisiting , file = "Visiting.csv")
 
